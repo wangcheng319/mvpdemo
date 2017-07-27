fe01c1212249945a882e3f3f42fd2a2fc81b9b4e
王成 <274281610@qq.com>
2017-07-27 18:02:18 +0800

    ~~~

--- android/app/src/main/java/vico/xin/mvpdemo/activity/ViewTestActivity.java
+++ android/app/src/main/java/vico/xin/mvpdemo/activity/ViewTestActivity.java
@@ -1,7 +1,9 @@
 package vico.xin.mvpdemo.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import vico.xin.mvpdemo.R;
 
@@ -11,5 +13,12 @@ public class ViewTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_test);
+
+        findViewById(R.id.btn_to_react).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ViewTestActivity.this,MyReactActivity.class));
+            }
+        });
     }
 }
